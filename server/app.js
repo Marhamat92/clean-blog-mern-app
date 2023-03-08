536166f29c9aba3eeec53dc5d3bd094c91dcc627
Marhamat <75548742+Marhamat92@users.noreply.github.com>
2023-03-08 22:08:44 +0300

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+const cors = require('cors');
+const session = require('express-session');  //we will use this to store user session
+const MongoDBStore = require('connect-mongodb-session')(session); //this is used to store session in mongodb
+const config = require('config');
+const db = config.get('mongoURI');
+
+const EXP_TIME = 1000 * 60 * 60 * 3; //this is the expiration time for the session in milliseconds (3 hours)
+
+const mongoDBstore = new MongoDBStore({
+  uri: db,
+  collection: 'mySessions'
+})
+
+//setting up session middleware
+app.use(session({
+  secret: 'BtTu5fE8fr0E6EL9CGvMFMhovqtDtvI9',
+  name: 'session-id',
+  store: mongoDBstore,
+  cookie: {
+    originalMaxAge: EXP_TIME,
+    sameSite: false,
+    secure: false
+  },
+  resave: true,
+  saveUninitialized: false,
+}))
+
+
+const loginRouter = require('./routes/api/loginRouter');
+
+
+const corsOptions = {
+  origin: 'http://localhost:5000',
+  optionsSuccessStatus: 200,
+  origin: true,
+  credentials: true
+}
+
+// cors
+app.use(cors(corsOptions));
+app.use(express.json({ extended: false }));
+app.use('/api', loginRouter);
+
+
+
+
+
+connectDB();
+
+
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+})
---
+++ server/models/Post.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+
+const PostSchema = new mongoose.Schema({
+  postedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  post_title: {
+    type: String,
+    required: true
+  },
+  post_subtitle: {
+    type: String,
+    required: true
+  },
+  post_content: {
+    type: String,
+    required: true
+  },
+  post_image: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Post = mongoose.model('post', PostSchema);
